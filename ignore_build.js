455a72fc534034489848ea0433d3cdaa2b2b1e41
JGAntunes <joao@netlify.com>
2022-10-10 18:52:21 +0100

    better logging

--- ignore_build.js
+++ ignore_build.js
@@ -3,10 +3,15 @@ import { exec } from "node:child_process";
 
 const { CACHED_COMMIT_REF, COMMIT_REF } = process.env;
 
+function log(data) {
+  console.log(`[build-ignore] ${data}`);
+}
+
 function run_diff() {
+  log("Starting ignore script");
   // Handle edge case where we're building without cache, in which case CACHED_COMMIT_REF will be the same as COMMIT_REF
   if (CACHED_COMMIT_REF === COMMIT_REF) {
-    console.log(
+    log(
       `Building with fresh cache. Cached commit ref - ${CACHED_COMMIT_REF} - is the same as this commit ref - ${COMMIT_REF}`
     );
     process.exitCode = 1;
@@ -15,10 +20,10 @@ function run_diff() {
 
   exec(`git diff ${CACHED_COMMIT_REF} ${$COMMIT_REF} -- dist`, (error) => {
     if (error) {
-      console.log(`Git diff returned ${error.code}`);
+      log(`Git diff returned ${error.code}`);
       process.exitCode = 1;
     }
-    console.log("Skipping build, no changes detected");
+    log("Skipping build, no changes detected");
   });
 }
 
