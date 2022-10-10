1b6769911c5108451f574a45b38795b65c149c6a
JGAntunes <joao@netlify.com>
2022-10-10 18:59:16 +0100

    make dirs to check configurable

--- ignore_build.js
+++ ignore_build.js
@@ -1,7 +1,11 @@
 import process from "node:process";
 import { exec } from "node:child_process";
 
-const { CACHED_COMMIT_REF, COMMIT_REF } = process.env;
+const { CACHED_COMMIT_REF, COMMIT_REF, DIRS_TO_CHECK } = process.env;
+
+// Directories to run git diff on. Defaults to dist and can be overridden via env var DIRS_TO_CHECK
+// e.g. DIRS_TO_CHECK="one_dir/ another_dir/" node ignore_build.js
+const DIRS_TO_CHECK = DIRS_TO_CHECK || "dist";
 
 function log(data) {
   console.log(`[build-ignore] ${data}`);
@@ -18,13 +22,17 @@ function run_diff() {
     return;
   }
 
-  exec(`git diff ${CACHED_COMMIT_REF} ${COMMIT_REF} -- dist`, (error) => {
-    if (error) {
-      log(`Git diff returned ${error.code}`);
-      process.exitCode = 1;
+  log(`Running git diff for the following directories: ${DIRS_TO_CHECK}`);
+  exec(
+    `git diff ${CACHED_COMMIT_REF} ${COMMIT_REF} -- ${DIRS_TO_CHECK}`,
+    (error) => {
+      if (error) {
+        log(`Git diff returned ${error.code}`);
+        process.exitCode = 1;
+      }
+      log("Skipping build, no changes detected");
     }
-    log("Skipping build, no changes detected");
-  });
+  );
 }
 
 run_diff();
